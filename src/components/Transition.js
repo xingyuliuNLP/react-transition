f3bff35c6839c0ccf1b6da7484ae179b0d3656cd
xingyuliuNLP <xingyuliufrance@gmail.com>
2019-05-29 20:31:51 +0200

    add style

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 import '../css/App.css';
 
-class TransitionComp extends Component{
+class TransitionComp extends Component {
+  state = {
+    show: false
+  }
+  showDiv = () => {
+    this.setState({
+      show: !this.state.show ? true : false
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Transition
+          in={this.state.show}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit>
+          {state =>
+            <div style={{
+              background: 'grey',
+              height: '100px',
+              transition: 'all 1s ease',
+              opacity: state === 'exited' || state === 'exiting' ? 0 : 1
+            }}>
+              { state }
+            </div>}
 
-    render(){
-        return(
-            <div>
-                Transition
-            </div>
+        </Transition>
+        
+        <div className="showDiv" onClick={this.showDiv}>
+          Show/Hide
+                </div>
+      </div>
 
-        )
-    }
+    )
+  }
 }
 
 
